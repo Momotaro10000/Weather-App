0063201f94b4512fac99794a95cfa6b01dc34cff
Momotaro10000 <nakanomei@gmail.com>
2022-10-10 17:50:39 +0200

    fixed language

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ LOCATIONS = ['London', 'svo', 'Cherepovets']
 def what_weather_is_in(location):
     weather_parameters = {
         'nmTq': '',
-        'lang': 'en'
+        'lang': 'ru'
     }
     response = requests.get(url=f'http://wttr.in/{location}',params=weather_parameters)
     response.raise_for_status()
